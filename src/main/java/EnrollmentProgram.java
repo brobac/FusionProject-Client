0f3fc26f781cb9ebd7b668e8f70bc25cbf4d9d32
eubin001122 <lwb001122@naver.com>
2021-11-28 08:37:46 +0900

    run() & login() modification

--- src/main/java/EnrollmentProgram.java
+++ src/main/java/EnrollmentProgram.java
@@ -3,43 +3,74 @@ import dto.AccountDTO;
 import network.Protocol;
 
 import java.io.*;
-import java.net.Socket;
+import java.util.Scanner;
 
 public class EnrollmentProgram {
-    private static Socket socket;
     private static InputStream is;
     private static OutputStream os;
 
-    public static void run(String address, int portNumber) throws IOException, IllegalAccessException, NoSuchMethodException, InstantiationException {
-        socket = new Socket(address, portNumber);
-        is = socket.getInputStream();
-        os = socket.getOutputStream();
-        //login 해서 응답받기
-        if (login()) {
-            EnrollmentService service = createService("admin", is, os);
-            service.run();
-        }
-        //직책에 따라 다른 서비스 실행
+    public EnrollmentProgram(InputStream is, OutputStream os) {
+        this.is = is;
+        this.os = os;
     }
-    private static AccountDTO login() throws IOException, IllegalAccessException {
+
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("************* 수강신청 프로그램 **************");
+        try {
+            String user;
+            while (true)
+            {
+                System.out.println(Message.ENROLLMENT_MENU);  // 로그인 or 종료 선택
+                int menu = sc.nextInt();
+                if (menu == 1)
+                {
+                    user = login();
+                    if (user == null)
+                        continue;
+                }
+                else if (menu == 2)
+                    break;
+                else
+                    continue;
+
+                EnrollmentService service = createService(user, is, os);
+                service.run();
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+
+    private String login() throws Exception{
         while (true) {
             BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("아이디 : ");
+            System.out.print(Message.ID_INPUT);
             String id = userIn.readLine();
-            System.out.print("비밀번호 : ");
+            System.out.print(Message.PW_INPUT);
             String pw = userIn.readLine();
             AccountDTO accountDTO = AccountDTO.builder()
                     .id(id)
                     .password(pw)
                     .build();
-            Protocol protocol = new Protocol(Protocol.TYPE_REQUEST, Protocol.T1_CODE_CREATE, Protocol.ENTITY_ACCOUNT);
+            Protocol protocol = new Protocol(Protocol.TYPE_REQUEST, Protocol.T1_CODE_LOGIN);
             protocol.setObject(accountDTO);
-            os.write(protocol.getPacket());
+            protocol.send(os);
 
+            Protocol recv = new Protocol();
+            recv.read(is);
 
-            //TODO 로그인 정보 생성 및 패킷 전송
+            if (recv.getType() == Protocol.TYPE_RESPONSE) {
+                if (recv.getCode() == Protocol.T2_CODE_SUCCESS)
+                    return accountDTO.getPosition();    // 어떤 사용자인지 return
+                else if (recv.getCode() == Protocol.T2_CODE_FAIL) // 로그인 실패시 null return
+                    return null;
+            } else
+                throw new Exception("서버에서 요청이 왜 와???"); //TODO 최종본에선 지울것
         }
     }
+
     private static EnrollmentService createService(String authority, InputStream is, OutputStream os) {
         if (authority.equals("admin")) {
             return new AdminService(is, os);
@@ -47,8 +78,6 @@ public class EnrollmentProgram {
             return new ProfessorService(is, os);
         } else if (authority.equals("student")) {
             return new StudentService(is, os);
-        } else {
-            return null;
         }
     }
 }
